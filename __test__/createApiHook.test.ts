51cff7225d4b48e1f79b6e111b21412d1aa331bc
Kazachenko Viacheslav <kreeon@ya.ru>
2020-10-06 23:36:55 +0300

    add test

--- __test__/createApiHook.test.ts
+++ __test__/createApiHook.test.ts
@@ -2,14 +2,25 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import { createApiHook } from "../src/createApiHook";
 
 const data = {
-    1: { value: "first" },
-    2: { value: "second" },
+    "1": { value: "first" },
+    "2": { value: "second" },
 };
 
 const getData = () =>
     new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(data);
+        }, 0);
+    });
+
+const getValue = (id: keyof typeof data) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (id in data) {
+                resolve(data[id].value);
+            } else {
+                reject("Error");
+            }
         }, 0);
     });
 
@@ -52,6 +63,23 @@ test("createApiHook tests immediate", async () => {
 
     expect(result.current.data).toHaveProperty("1", { value: "first" });
     expect(result.current.data).toHaveProperty("2", { value: "second" });
+    expect(result.current.pending).toBe(false);
+    expect(result.current.error).toBeNull();
+});
+
+test("createApiHook tests immediate should get value", async () => {
+    const useData = createApiHook(getValue);
+    const { result, waitForNextUpdate } = renderHook(() =>
+        useData({ loadImmediately: true, args: ["1"] })
+    );
+
+    expect(result.current.data).toBeNull();
+    expect(result.current.error).toBeNull();
+    expect(result.current.pending).toBe(true);
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toBe("first");
     expect(result.current.pending).toBe(false);
     expect(result.current.error).toBeNull();
 });
